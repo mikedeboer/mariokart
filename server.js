256ae7df1bae78ce516579552e2a6b5b36f5cd05
Mike de Boer <info@mikedeboer.nl>
2011-06-10 14:55:29 +0000

    fixed static and paths

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var Connect = require("connect");
 var SocketIO = require("socket.io");
 
 var server = Connect.createServer(
-    Connect.staticProvider(__dirname + "/public_html"),
-    Connect.staticProvider(__dirname + "/Socket.IO/lib")
+    Connect.static(__dirname + "/public_html"),
+    Connect.static(__dirname + "/Socket.IO/lib")
 );
 server.listen(process.env.C9_PORT, "0.0.0.0");
 
-var Server = require("server/index");
+var Server = require("./server/index");
 var playerServer = new Server();
 
 var socket = SocketIO.listen(server);
